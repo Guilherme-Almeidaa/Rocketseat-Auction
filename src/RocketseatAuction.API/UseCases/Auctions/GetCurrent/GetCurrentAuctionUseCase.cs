acbbcaa996979daae237e5a1bb4aa026320b3301
Guilherme <guilherme.almeida@rehagro.edu.br>
2024-02-06 19:51:26 -0300

    initial commit

---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction? Execute()
+        {
+            var respository = new RocketseatAuctionDbContext();
+            var today = DateTime.Now;
+            return respository
+                .Auctions
+                .Include(Auction =>  Auction.Items)
+                .FirstOrDefault(auction =>  today >= auction.Starts && today <= auction.Ends);
+        }
+    }
+}
